a54898dab1cf8151355c1a78ad589e6a58e629cc
riyanakbari <rynakbri@gmail.com>
2024-05-22 23:11:44 +0700

    add: active navbar

--- assets/script.js
+++ assets/script.js
@@ -18,3 +18,24 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav .container ul li");
+
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 200) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.id === `${current}-li`) {
+      li.classList.add("active");
+    }
+  });
+};
